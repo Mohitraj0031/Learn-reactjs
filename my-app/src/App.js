3f6cec35524c237993cc5ead72526b9b735d36df
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-11 08:14:57 +0000

    convert to upper case

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
 import Navbar from './component/Navbar';
+import Textform from './component/Textform';
 
 function App() {
   return (
     <>
   
-  {/* <Navbar title = "TextUtile2" AboutText = "About us" /> */}
-  <Navbar />
+  <Navbar title = "TextUtile2" AboutText = "About us" />
+  {/* <Navbar /> */}
+  
+  <div className= "container" >
+     <Textform heading = "Enter the text to Analyse" />
+  </div>
+ 
   </>
   );
 }

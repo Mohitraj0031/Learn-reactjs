3fb85440193d4680227b4b8fc27daa91a1667b13
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-12 12:45:42 +0000

    class Renamed

--- Projects/1-todoList-one/src/App.jsx
+++ Projects/1-todoList-one/src/App.jsx
@@ -3,13 +3,13 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import Todoitem1 from "./components/Todoitem1";
 import Todoitem2 from "./components/Todoitem2";
-
+import "./App.css";
 
 
 function App() {
 
 
-  return <center class="todo-container">
+  return <center className="todo-container">
 
     <AppName/>
     <AddTodo/>
--- Projects/1-todoList-one/src/components/Todoitem1.jsx
+++ Projects/1-todoList-one/src/components/Todoitem1.jsx
@@ -4,16 +4,16 @@ function Todoitem1(){
     let todoDate = "4/10/2024";
 
     return(
-        <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
+        <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
             {todoName}
           </div>
-          <div class="col-4">
+          <div className="col-4">
             {todoDate}
           </div>
-          <div class="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
+          <div className="col-2">
+          <button type="button" className="btn btn-danger">Delete</button>
           </div>
         </div>
      </div>   

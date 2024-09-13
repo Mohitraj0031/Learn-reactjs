a8b080e5fd92d6ebc22171bf54214df511c99791
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-13 04:32:38 +0000

    Bharat Project completed

--- Projects/2-BharatCLock/src/App.jsx
+++ Projects/2-BharatCLock/src/App.jsx
@@ -1,10 +1,19 @@
+
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import ClockTimer from "./components/ClockTimer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
  
 
   return (
-   <div>Clock</div>
+   <center>
+    <ClockHeading/>
+    <ClockSlogan/>
+    <ClockTimer/>
+   </center>
   )
 }
 

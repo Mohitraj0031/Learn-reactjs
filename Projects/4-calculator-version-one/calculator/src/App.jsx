d9c7f015cb60e90f09383e2a524b5596be4d6689
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-15 09:03:27 +0000

    calculator project done

---
+++ Projects/4-calculator-version-one/calculator/src/App.jsx
@@ -0,0 +1,17 @@
+
+import css from "./App.module.css"
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+
+function App() {
+ 
+
+  return <div className={css.calculator}>
+    <Display></Display>
+    <ButtonsContainer></ButtonsContainer>
+  </div>
+   
+  
+}
+
+export default App

1761eda71fd0eaa2db4dc5f5cafb6149be9dbdb5
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-17 10:51:10 +0000

    Add

--- Projects/5-calculator-version-Two/calculator/src/App.jsx
+++ Projects/5-calculator-version-Two/calculator/src/App.jsx
@@ -2,13 +2,14 @@
 import css from "./App.module.css"
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 
-function App() {
+function App() { 
    let [calVal,setcalVal] = useState("");
 
   return <div className={css.calculator}>
     <Display displayValue = {calVal}></Display>
-    <ButtonsContainer></ButtonsContainer>
+    <ButtonsContainer onButtonClick={() => console.log("button clicked.")}></ButtonsContainer>
   </div>
    
   

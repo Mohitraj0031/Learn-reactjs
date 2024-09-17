5e5223b8878e8b038beac97fe923674392a05a68
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-17 10:51:23 +0000

    Add more

--- Projects/5-calculator-version-Two/calculator/src/components/ButtonsContainer.jsx
+++ Projects/5-calculator-version-Two/calculator/src/components/ButtonsContainer.jsx
@@ -2,10 +2,10 @@ import css from "./ButtonsContainer.module.css"
 
 const buttonNames = ['C','1','2','+','3','4','-','5','6','*','7','8','/',"=",'9','0','.']
 
-const ButtonsContainer = () =>{
+const ButtonsContainer = ({ onButtonClick }) =>{
  return  <div className={css.buttonsContainer}>
   
-  {buttonNames.map(buttonName =>  <button className={css.button}>{buttonName}</button>)}
+  {buttonNames.map(buttonName =>  <button className={css.button} onClick={onButtonClick}>{buttonName}</button>)}
 </div>
 }
 
--- Projects/5-calculator-version-Two/calculator/src/components/Display.jsx
+++ Projects/5-calculator-version-Two/calculator/src/components/Display.jsx
@@ -1,7 +1,7 @@
 import css from "./Display.module.css"
 
-const Display = (displayValue)=>{
-    return  <input className={css.display} type="text" value={displayValue} />;
+const Display = ({displayValue})=>{
+    return  <input className={css.display} type="text" value={displayValue} readOnly />;
     
 }
 

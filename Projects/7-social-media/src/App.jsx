a347a48fa95077b880f32072db49ca3cb2c69737
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-21 12:51:04 +0000

    Social Media Project

---
+++ Projects/7-social-media/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Sidebar from './component/Sidebar';
+
+function App() {
+  
+  return(
+  
+  <>
+      <Sidebar></Sidebar>
+      <Header></Header>
+      <Footer></Footer>
+     
+  </>
+
+
+   );
+
+}
+  
+export default App

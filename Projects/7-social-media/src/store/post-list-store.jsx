0841ac34621b56384b5a9ac1ca57bc99cd4e33fa
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-09-22 12:29:55 +0000

    add Social media Project

---
+++ Projects/7-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+
+ const PostList = createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () =>{},
+ });
+
+ const PostListProvider = ({children}) => {
+
+   const [postList,dispatchPostList] = useReducer();
+    
+   const addPost = () =>{
+
+   }
+
+   const deletePost = () =>{
+
+   }
+   
+   return <PostList.Provider value={{postList, addPost,deletePost}} >{children}</PostList.Provider>
+ }
+
+ export default PostListProvider;

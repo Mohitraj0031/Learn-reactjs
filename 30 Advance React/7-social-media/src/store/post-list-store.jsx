b7368e931a29a48f6d6acb1854f7378a629ee370
Mohitraj0031 <promosarthi91@gmail.com>
2024-09-24 19:13:34 +0530

    Fetching data done

---
+++ 30 Advance React/7-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,68 @@
+import { createContext, useReducer } from "react";
+ 
+export const PostList = createContext({
+
+  postList: [],
+  addPost: () => {},
+  addInitailPosts: () => {},
+  deletePost: () => {},
+  
+});
+
+const postListReducer = (currPostList,action) =>{
+  let newPostList = currPostList;
+  if(action.type === "DELETE_POST"){
+    newPostList = currPostList.filter(post => post.id !== action.payload.postId);
+  }else if(action.type === "ADD_POST"){
+    newPostList = [action.payload,...currPostList];
+  }else if(action.type === "ADD_INITIAL_POSTS"){
+    newPostList = action.payload.posts;
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
+
+  const addPost = (userId,postTitle,postBody,likes,dislikes, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload:{
+        id: Date.now(),
+        title: postTitle,
+        body:postBody,
+        reactions:{likes,dislikes},
+        userId:userId,
+        tags:tags,
+      }
+    })
+  };
+
+  const addInitailPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payload:{
+       posts,
+      }
+    })
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    })
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, addInitailPosts, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+
+
+export default PostListProvider;

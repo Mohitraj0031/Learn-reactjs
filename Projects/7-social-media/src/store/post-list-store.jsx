f765ebd84fd1b4cfd03b223f03edd9a4f3fc5abb
Mohitraj0031 <promosarthi91@gmail.com>
2024-09-24 12:30:03 +0530

    Social Media version one

--- Projects/7-social-media/src/store/post-list-store.jsx
+++ Projects/7-social-media/src/store/post-list-store.jsx
@@ -1,24 +1,51 @@
 import { createContext, useReducer } from "react";
+ 
+export const PostList = createContext({
 
- const PostList = createContext({
-    postList: [],
-    addPost: () => {},
-    deletePost: () =>{},
- });
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
 
- const PostListProvider = ({children}) => {
+});
 
-   const [postList,dispatchPostList] = useReducer();
-    
-   const addPost = () =>{
+const postListReducer = (currPostList,action) =>{
+  return currPostList;
+}
 
-   }
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
-   const deletePost = () =>{
+  const addPost = () => {};
 
-   }
-   
-   return <PostList.Provider value={{postList, addPost,deletePost}} >{children}</PostList.Provider>
- }
+  const deletePost = () => {};
 
- export default PostListProvider;
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id:"1",
+    title: "Going to Mumbai",
+    body:"Hi Friends, I am going to Mumbai",
+    reactions:2,
+    userId:"user-9",
+    tags:["vaccation","mumbai","enjoying"],
+
+  },
+
+  {
+    id:"2",
+    title: "Pass Ho gaye",
+    body:"Hi Friends, finally graduation complete ho gayi",
+    reactions:15,
+    userId:"user-12",
+    tags:["Graduating","Unbelievable"],
+
+  },
+];
+
+export default PostListProvider;

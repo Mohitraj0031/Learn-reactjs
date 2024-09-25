3b1a29e5debf22749622c1f70f47089ebf70e60d
Mohitraj0031 <promosarthi91@gmail.com>
2024-09-25 18:01:42 +0530

    bharat clock updated

---
+++ Projects/8-BharatCLock-version-two/src/App.jsx
@@ -0,0 +1,20 @@
+
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import ClockTimer from "./components/ClockTimer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+   <center>
+    <ClockHeading/>
+    <ClockSlogan/>
+    <ClockTimer/>
+   </center>
+  )
+}
+
+export default App
---
+++ Projects/8-BharatCLock-version-two/src/components/ClockSlogan.jsx
@@ -0,0 +1,5 @@
+let ClockSlogan = ()=>{
+    return <p className="fst-italic">This is the Clock that show Time in
+         Bharat at all times</p>
+}
+export default ClockSlogan;

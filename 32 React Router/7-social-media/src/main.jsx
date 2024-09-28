81ad8b193b8e4855c9b4aab391ce4576ed7a1a1a
Mohitraj0031 <promosarthi91@gmail.com>
2024-09-28 11:49:29 +0530

    Route-routing, loader funciton

---
+++ 32 React Router/7-social-media/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./router/App.jsx";
+import CreatePost from "./component/CreatePost.jsx";
+import  PostList,{postLoader} from "./component/PostList.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList/>, loader: postLoader },
+      { path: "/create-post", element: <CreatePost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
